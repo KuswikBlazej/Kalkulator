bfe7c8d5f2dacb4b656a1935134e46b9724fc2ab
Błażej Kuświk <bkuswik@s201.local>
2024-10-21 08:29:13 +0200

    Dodano v2

---
+++ Kalkulator/Kalkulator/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Calculator.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Wyświetlenie nagłowka - opis aplikacji
+            //2. Prośba o podanie 1 liczby
+            //3. Pobranie liczby od użytkownika
+            //4. Prośba o podanie działania
+            //5. Pobranie wybranego działania od użytkownika
+            //6. Prośba o podanie 2 liczby
+            //7. Pobranie liczby od użytkownika
+            //8. Wykonanie obliczeń
+            //9. Wyświetlenie wyniku użytkownikowi
+        }
+    }
+}
