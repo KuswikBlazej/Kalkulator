a9548a530d765362bdb3ce8e496a33914a360e5b
Błażej Kuświk <bkuswik@s201.local>
2024-10-21 08:38:57 +0200

    Dodano pobranie 1 liczby od użytkownika

--- Kalkulator/Kalkulator/Program.cs
+++ Kalkulator/Kalkulator/Program.cs
@@ -6,7 +6,12 @@
         {
             //1. Wyświetlenie nagłowka - opis aplikacji
             Console.WriteLine("Witaj w aplikacji KALKULATOR!");
+
             //2. Prośba o podanie 1 liczby
+            Console.WriteLine("Podaj proszę 1 liczbę:");
+
+            //3. Pobranie liczby od użytkownika
+            var int1 = Console.ReadLine();
             //3. Pobranie liczby od użytkownika
             //4. Prośba o podanie działania
             //5. Pobranie wybranego działania od użytkownika
