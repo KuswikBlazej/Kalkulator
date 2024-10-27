270c0a6b023a43dba83019bff9a8872839a06c09
Blazej Kuswik <blazejkuswik@gmail.com>
2024-10-27 10:09:10 +0100

    Dodano obsługe błędów

--- Kalkulator/Kalkulator/Program.cs
+++ Kalkulator/Kalkulator/Program.cs
@@ -4,50 +4,61 @@
     {
         static void Main(string[] args)
         {
-            //1. Wyświetlenie nagłowka - opis aplikacji
-            Console.WriteLine("Witaj w aplikacji KALKULATOR!");
-
-            //2. Prośba o podanie 1 liczby
-            Console.WriteLine("Podaj proszę 1 liczbę:");
-
-            //3. Pobranie liczby od użytkownika
-            var int1 = int.Parse(Console.ReadLine());
-
-            //4. Prośba o podanie działania
-            Console.WriteLine("Jaką operację chcesz wykonać? Możliwe operacje to: '+', '-', '*', '/'.");
-
-            //5. Pobranie wybranego działania od użytkownika
-            var op = Console.ReadLine();
-
-            //6. Prośba o podanie 2 liczby
-            Console.WriteLine("Podaj proszę 2 liczbę:");
-
-            //7. Pobranie liczby od użytkownika
-            var int2 = int.Parse(Console.ReadLine());
-
-            //8. Wykonanie obliczeń
-            var res = 0;
-
-            switch (op)
+            try
             {
-                case "+":
-                    res = int1 + int2;
-                    break;
-                case "-":
-                    res = int1 - int2;
-                    break;
-                case "*":
-                    res = int1 * int2;
-                    break;
-                case "/":
-                    res = int1 / int2;
-                    break;
-                default:
-                    throw new Exception("Wybrałeś złą operację!");
-            }
 
-            //9. Wyświetlenie wyniku użytkownikowi
-            Console.WriteLine($"Wynik Twojego działania to: {res}.");
+                //1. Wyświetlenie nagłowka - opis aplikacji
+                Console.WriteLine("Witaj w aplikacji KALKULATOR!");
+
+                //2. Prośba o podanie 1 liczby
+                Console.WriteLine("Podaj proszę 1 liczbę:");
+
+                //3. Pobranie liczby od użytkownika
+                var int1 = int.Parse(Console.ReadLine());
+
+                //4. Prośba o podanie działania
+                Console.WriteLine("Jaką operację chcesz wykonać? Możliwe operacje to: '+', '-', '*', '/'.");
+
+                //5. Pobranie wybranego działania od użytkownika
+                var op = Console.ReadLine();
+
+                //6. Prośba o podanie 2 liczby
+                Console.WriteLine("Podaj proszę 2 liczbę:");
+
+                //7. Pobranie liczby od użytkownika
+                var int2 = int.Parse(Console.ReadLine());
+
+                //8. Wykonanie obliczeń
+                var res = 0;
+
+                switch (op)
+                {
+                    case "+":
+                        res = int1 + int2;
+                        break;
+                    case "-":
+                        res = int1 - int2;
+                        break;
+                    case "*":
+                        res = int1 * int2;
+                        break;
+                    case "/":
+                        res = int1 / int2;
+                        break;
+                    default:
+                        throw new Exception("Wybrałeś złą operację!");
+                }
+
+                //9. Wyświetlenie wyniku użytkownikowi
+                Console.WriteLine($"Wynik Twojego działania to: {res}.");
+
+            }
+            catch (Exception ex)
+            {
+                //logowanie do pliku
+                Console.WriteLine(ex.Message);
+            }
         }
+
     }
 }
